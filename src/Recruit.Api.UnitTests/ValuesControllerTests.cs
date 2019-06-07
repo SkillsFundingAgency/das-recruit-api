29fcfdd1e8a217aaa18e1d7adfbd1b4d15ef01af
sachin.patel <sachin.patel@valtech.co.uk>
2019-06-07 07:33:01 +0100

    ER-1261 💀 - Skeleton Recruit API app

---
+++ src/Recruit.Api.UnitTests/ValuesControllerTests.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+using FluentAssertions;
+using SFA.DAS.Recruit.Api.Controllers;
+using Xunit;
+
+namespace SFA.DAS.Recruit.Api.UnitTests
+{
+    public class ValuesControllerTests
+    {
+        [Fact]
+        public void GetCall_ReturnsTwoTestStrings()
+        {
+            var ctrlr = new ValuesController();
+            var result = ctrlr.Get();
+            result.Value.Count().Should().Be(2);
+        }
+    }
+}
---
+++ src/Recruit.Api/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using SFA.DAS.Configuration.AzureTableStorage;
+
+namespace SFA.DAS.Recruit.Api
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile("appsettings.Development.json", true)
+                //.AddEnvironmentVariables()   // do we need to inspect any environment variables???
+                // .AddAzureTableStorage(
+                //     configuration["ConfigurationStorageConnectionString"],
+                //     configuration["ConfigNames"],
+                //     configuration["Environment"],
+                //     configuration["Version"]
+                // )
+                .Build();
+
+            Configuration = config;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
