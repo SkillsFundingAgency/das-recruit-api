29b8d521f736677bdf9976aea5f7c7a7df710045
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2025-06-10 18:25:51 +0100

    FAI-2617 - Add endpoint for getting application review by application id

---
+++ src/Recruit.Api.UnitTests/Controllers/ApplicationReviewControllerTests/WhenGettingTheApplicationReviewByApplicationId.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using SFA.DAS.Recruit.Api.Application.Providers;
+using SFA.DAS.Recruit.Api.Controllers;
+using SFA.DAS.Recruit.Api.Domain.Entities;
+using SFA.DAS.Recruit.Api.Models.Mappers;
+using SFA.DAS.Recruit.Api.Models.Responses.ApplicationReview;
+
+namespace SFA.DAS.Recruit.Api.UnitTests.Controllers.ApplicationReviewControllerTests;
+
+internal class WhenGettingTheApplicationReviewByApplicationId
+{
+    [Test, MoqAutoData]
+    public async Task Get_ReturnsOk_WhenApplicationReviewExists(
+        Guid applicationId,
+        ApplicationReviewEntity mockResponse,
+        [Frozen] Mock<IApplicationReviewsProvider> provider,
+        [Greedy] ApplicationReviewController controller,
+        CancellationToken token)
+    {
+        // Arrange
+        provider.Setup(p => p.GetByApplicationId(applicationId, token)).ReturnsAsync(mockResponse);
+
+        // Act
+        var result = await controller.GetByApplicationId(applicationId, token);
+
+        // Assert
+        result.Should().BeOfType<Ok<GetApplicationReviewResponse>>();
+        var okResult = result as Ok<GetApplicationReviewResponse>;
+        okResult!.Value.Should().BeEquivalentTo(mockResponse.ToPutResponse());
+    }
+
+    [Test, MoqAutoData]
+    public async Task Get_ReturnsNotFound_WhenApplicationReviewDoesNotExist(
+        Guid applicationId,
+        ApplicationReviewEntity mockResponse,
+        [Frozen] Mock<IApplicationReviewsProvider> provider,
+        [Greedy] ApplicationReviewController controller,
+        CancellationToken token)
+    {
+        // Arrange
+        provider.Setup(p => p.GetByApplicationId(applicationId, token)).ReturnsAsync((ApplicationReviewEntity)null!);
+
+        // Act
+        var result = await controller.GetByApplicationId(applicationId, token);
+
+        // Assert
+        result.Should().BeOfType<NotFound>();
+    }
+
+    [Test, MoqAutoData]
+    public async Task Get_ReturnsInternalServerException_WhenException_Thrown(
+        Guid applicationId,
+        ApplicationReviewEntity mockResponse,
+        [Frozen] Mock<IApplicationReviewsProvider> provider,
+        [Greedy] ApplicationReviewController controller,
+        CancellationToken token)
+    {
+        // Arrange
+        provider.Setup(p => p.GetByApplicationId(applicationId, token)).ThrowsAsync(new Exception());
+
+        // Act
+        var result = await controller.GetByApplicationId(applicationId, token);
+
+        // Assert
+        result.Should().BeOfType<ProblemHttpResult>();
+    }
+}
