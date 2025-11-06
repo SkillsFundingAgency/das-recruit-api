fe4419202be2c106defc475825d641b36bb6b2c6
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2025-11-06 16:17:37 +0000

    missed comma in the sql statement

--- src/Recruit.Api.Database/Tables/Vacancy.sql
+++ src/Recruit.Api.Database/Tables/Vacancy.sql
@@ -105,6 +105,6 @@ CREATE TABLE dbo.[Vacancy] (
     INDEX [IX_Vacancy_ProviderReview] NONCLUSTERED(AccountId, CreatedDate DESC)  INCLUDE([Id], [Title], [VacancyReference], [ClosingDate], [ApplicationMethod], [ApprenticeshipType],  [LegalEntityName], [TransferInfo], [HasSubmittedAdditionalQuestions], [Status], [OwnerType],[Ukprn]) WHERE OwnerType = 'Provider' AND Status = 'Review',
     
     INDEX [IX_Vacancy_Search_Provider] NONCLUSTERED([OwnerType], [Ukprn], [Status]) INCLUDE ([LegalEntityName], [Title], [VacancyReference]),
-    INDEX [IX_Vacancy_Search_Employer] NONCLUSTERED([OwnerType], [AccountId], [Status]) INCLUDE ([LegalEntityName], [Title], [VacancyReference])
+    INDEX [IX_Vacancy_Search_Employer] NONCLUSTERED([OwnerType], [AccountId], [Status]) INCLUDE ([LegalEntityName], [Title], [VacancyReference]),
     INDEX [IX_Vacancy_Report_Provider] NONCLUSTERED([Ukprn], [Status]) INCLUDE ([Title], [EmployerName], [ApprenticeshipType], [EmployerLocationOption], [ProgrammeId], [TrainingProvider_Name], [ClosingDate], [VacancyReference]) WHERE Status IN ('Live','Closed') AND OwnerType = 'Provider',
     )
