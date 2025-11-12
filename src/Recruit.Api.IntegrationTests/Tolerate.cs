b8d8d46e66d061a3e339f473bc9759fa7b5e4b64
killij <51908793+killij@users.noreply.github.com>
2025-11-12 16:32:39 +0000

    FAI-2856 Add Tolerate to allow for handling the precision loss when date times are saved/returned from SQL Server

---
+++ src/Recruit.Api.IntegrationTests/Tolerate.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions.Equivalency;
+
+namespace SFA.DAS.Recruit.Api.IntegrationTests;
+
+public static class Tolerate
+{
+    private const int Tolerance = 10;
+    
+    public static Func<EquivalencyAssertionOptions<T>, EquivalencyAssertionOptions<T>> SqlDateTime<T>()
+    {
+        return options => options
+            .Using<DateTime>(ctx => ctx.Subject.Should().BeCloseTo(ctx.Expectation, TimeSpan.FromMilliseconds(Tolerance)))
+            .WhenTypeIs<DateTime>();
+    }
+
+    public static EquivalencyAssertionOptions<T> TolerateSqlDateTime<T>(this EquivalencyAssertionOptions<T> options)
+    {
+        return options
+            .Using<DateTime>(ctx => ctx.Subject.Should().BeCloseTo(ctx.Expectation, TimeSpan.FromMilliseconds(Tolerance)))
+            .WhenTypeIs<DateTime>();
+    }
+}
