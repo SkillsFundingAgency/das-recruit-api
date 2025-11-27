89dee0b630d4e24b2f9b238b38b819622e6bd77f
killij <51908793+killij@users.noreply.github.com>
2025-11-27 14:41:06 +0000

    FAI-2843 Changes to schema

--- src/Recruit.Api.Database/Tables/BlockedOrganisation.sql
+++ src/Recruit.Api.Database/Tables/BlockedOrganisation.sql
@@ -1,12 +1,12 @@
 CREATE TABLE dbo.BlockedOrganisation (
    [Id]	                      uniqueidentifier NOT NULL,
-   [OrganisationId]           varchar(20) NOT NULL,
-   [OrganisationType]         varchar(20) NOT NULL,
-   [BlockedStatus]            varchar(20) NOT NULL,
-   [Reason]                   varchar(max) NOT NULL,
-   [UpdatedByUserId]          uniqueidentifier NOT NULL,
+   [OrganisationId]           nvarchar(20) NOT NULL,
+   [OrganisationType]         nvarchar(20) NOT NULL,
+   [BlockedStatus]            nvarchar(20) NOT NULL,
+   [Reason]                   nvarchar(max) NOT NULL,
+   [UpdatedByUserId]          nvarchar(255) NOT NULL,
+   [UpdatedByUserEmail]       nvarchar(255) NOT NULL,
    [UpdatedDate]              DATETIME NOT NULL,
    CONSTRAINT [PK_BlockedOrganisation] PRIMARY KEY (Id),
-   CONSTRAINT [FK_BlockedOrganisation_User] FOREIGN KEY (UpdatedByUserId) REFERENCES [User](Id),
    INDEX [IX_BlockedOrganisation_OrganisationId] NONCLUSTERED(OrganisationId) INCLUDE(Id, UpdatedByUserId, UpdatedDate, BlockedStatus, OrganisationType, Reason)
 )
