7cc1ff1affa95e9e014957d902c1650fe83000de
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2025-12-15 23:11:44 +0000

    FAI-2944 - Always include emails to employers
    This will be done while we fix the issue with the last signed in date not being recorded

--- src/Recruit.Api.UnitTests/Core/Email/WhenGettingUsersForNotificationType.cs
+++ src/Recruit.Api.UnitTests/Core/Email/WhenGettingUsersForNotificationType.cs
@@ -18,6 +18,62 @@ public class WhenGettingUsersForNotificationTyp
     }
 
     [Test, RecursiveMoqAutoData]
+    public void Then_Employers_Are_Included_And_Only_Active_Providers_Are_Returned(
+        UserEntity employerOne,
+        UserEntity employerTwo,
+        UserEntity providerOne,
+        UserEntity providerTwo,
+        UserEntity providerThree)
+    {
+        // arrange
+        var now = DateTime.UtcNow;
+        var preferences = new NotificationPreferences {
+            EventPreferences = [
+                new NotificationPreference(
+                    NotificationTypes.SharedApplicationReviewedByEmployer,
+                    "",
+                    NotificationScope.OrganisationVacancies,
+                    NotificationFrequency.Immediately)
+            ]
+        };
+        
+        employerOne.UserType = UserType.Employer;
+        employerOne.LastSignedInDate = now.AddYears(-2);
+        employerOne.NotificationPreferences = preferences;
+        employerTwo.UserType = UserType.Employer;
+        employerTwo.LastSignedInDate = null;
+        employerTwo.NotificationPreferences = preferences;
+        providerOne.UserType = UserType.Provider;
+        providerOne.LastSignedInDate = null;
+        providerOne.NotificationPreferences = preferences;
+        providerTwo.UserType = UserType.Provider;
+        providerTwo.LastSignedInDate = now.AddYears(-2);
+        providerTwo.NotificationPreferences = preferences;
+        providerThree.UserType = UserType.Provider;
+        providerThree.LastSignedInDate = now.AddMonths(-6);
+        providerThree.NotificationPreferences = preferences;
+        
+        var users = new List<UserEntity>
+        {
+            employerOne,
+            employerTwo,
+            providerOne,
+            providerTwo,
+            providerThree
+        };
+
+        // act
+        var result = users.GetUsersForNotificationType(NotificationTypes.SharedApplicationReviewedByEmployer);
+
+        // assert
+        result.Should().Contain(employerOne); 
+        result.Should().Contain(employerTwo); 
+        result.Should().Contain(providerThree);
+        result.Should().NotContain(providerOne);
+        result.Should().NotContain(providerTwo);
+    }
+
+    [Test, RecursiveMoqAutoData]
     public void Then_Users_Who_Do_Not_Want_The_Email_Are_Not_Returned(List<UserEntity> users)
     {
         // arrange
