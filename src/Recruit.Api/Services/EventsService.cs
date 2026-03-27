12980663d11ce7ce8fa9e7c2b91bf6b025436178
killij <51908793+killij@users.noreply.github.com>
2026-03-27 15:22:29 +0000

    FAI-3325 Publish new properties in the VacancyReviewCreatedEvent

--- src/Recruit.Api/Services/EventsService.cs
+++ src/Recruit.Api/Services/EventsService.cs
@@ -4,6 +4,7 @@ using SFA.DAS.Recruit.Api.Core.Events;
 using SFA.DAS.Recruit.Api.Domain.Configuration;
 using SFA.DAS.Recruit.Api.Domain.Entities;
 using SFA.DAS.Recruit.Api.Models;
+using SFA.DAS.Recruit.Api.Validators.Rules;
 using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace SFA.DAS.Recruit.Api.Services;
@@ -21,7 +22,10 @@ public class EventsService(ILogger<EventsService> logger, IMessageSession messag
         ArgumentNullException.ThrowIfNull(entity);
         var snapshot = JsonSerializer.Deserialize<VacancySnapshot>(entity.VacancySnapshot, JsonConfig.Options);
         logger.LogInformation("Publishing VacancyReviewCreatedEvent, vacancyReviewId='{VacancyReviewId}, vacancyId='{VacancyId}' ", entity.Id, snapshot!.Id);
-        await messageSession.Publish(new VacancyReviewCreatedEvent(snapshot.Id, entity.Id));
+
+        var isResubmission = entity.SubmissionCount > 1;
+        var hasPassedAutoQaChecks = string.Equals(entity.AutomatedQaOutcome, nameof(RuleSetDecision.Approve), StringComparison.InvariantCultureIgnoreCase);
+        await messageSession.Publish(new VacancyReviewCreatedEvent(snapshot.Id, entity.Id, isResubmission, hasPassedAutoQaChecks));
     }
     
     public async Task PublishVacancyClosedEvent(VacancyEntity entity)
