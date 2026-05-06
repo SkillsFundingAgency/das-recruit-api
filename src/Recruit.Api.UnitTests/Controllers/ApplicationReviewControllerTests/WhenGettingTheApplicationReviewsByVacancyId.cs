ed0bb651ae4282441091716c6e69bde4ec3b7505
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2026-05-06 10:44:19 +0100

    Added new endpoint to get all the application reviews by given vacancy Id

---
+++ src/Recruit.Api.UnitTests/Controllers/ApplicationReviewControllerTests/WhenGettingTheApplicationReviewsByVacancyId.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using SFA.DAS.Recruit.Api.Controllers;
+using SFA.DAS.Recruit.Api.Data.Providers;
+using SFA.DAS.Recruit.Api.Domain.Entities;
+using SFA.DAS.Recruit.Api.Models.Mappers;
+using SFA.DAS.Recruit.Api.Models.Responses.ApplicationReview;
+
+namespace SFA.DAS.Recruit.Api.UnitTests.Controllers.ApplicationReviewControllerTests;
+
+[TestFixture]
+internal class WhenGettingTheApplicationReviewsByVacancyId
+{
+    [Test, RecursiveMoqAutoData]
+    public async Task Get_ReturnsOk_WhenApplicationReviewExists(
+        Guid vacancyId,
+        List<ApplicationReviewEntity> mockResponse,
+        [Frozen] Mock<IApplicationReviewsProvider> provider,
+        [Greedy] ApplicationReviewController controller,
+        CancellationToken token)
+    {
+        // Arrange
+        provider.Setup(p => p.GetAllByVacancyId(vacancyId, token)).ReturnsAsync(mockResponse);
+
+        // Act
+        var result = await controller.GetManyByVacancyId(vacancyId, token);
+
+        // Assert
+        result.Should().BeOfType<Ok<List<GetApplicationReviewResponse>>>();
+        var okResult = result as Ok<List<GetApplicationReviewResponse>>;
+        okResult!.Value.Should().BeEquivalentTo(mockResponse.ToGetResponse());
+    }
+
+    [Test, RecursiveMoqAutoData]
+    public async Task Get_ReturnsNotFound_WhenApplicationReviewDoesNotExist(
+        Guid vacancyId,
+        List<ApplicationReviewEntity> mockResponse,
+        [Frozen] Mock<IApplicationReviewsProvider> provider,
+        [Greedy] ApplicationReviewController controller,
+        CancellationToken token)
+    {
+        // Arrange
+        provider.Setup(p => p.GetAllByVacancyId(vacancyId, token)).ReturnsAsync((List<ApplicationReviewEntity>)null!);
+
+        // Act
+        var result = await controller.GetManyByVacancyId(vacancyId, token);
+
+        // Assert
+        result.Should().BeOfType<NotFound>();
+    }
+
+    [Test, RecursiveMoqAutoData]
+    public async Task Get_ReturnsInternalServerException_WhenException_Thrown(
+        Guid vacancyId,
+        List<ApplicationReviewEntity> mockResponse,
+        [Frozen] Mock<IApplicationReviewsProvider> provider,
+        [Greedy] ApplicationReviewController controller,
+        CancellationToken token)
+    {
+        // Arrange
+        provider.Setup(p => p.GetAllByVacancyId(vacancyId, token)).ThrowsAsync(new Exception());
+
+        // Act
+        var result = await controller.GetManyByVacancyId(vacancyId, token);
+
+        // Assert
+        result.Should().BeOfType<ProblemHttpResult>();
+    }
+}
