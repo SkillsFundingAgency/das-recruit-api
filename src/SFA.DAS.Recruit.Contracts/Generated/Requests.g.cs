7a5fc4f11a08118692159d3e35cd2b58c1d3da12
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2026-05-08 19:54:56 +0100

    FAI-3027 - Update contracts project

--- src/SFA.DAS.Recruit.Contracts/Generated/Requests.g.cs
+++ src/SFA.DAS.Recruit.Contracts/Generated/Requests.g.cs
@@ -29,11 +29,11 @@ public class PutApplicationreviewsByApplicationIdApiRequest : IPutApiRequest<Put
 }
 
 /// <summary>PATCH /api/applicationreviews/{applicationId} &#x2192; <see cref="PatchApplicationReviewResponse"/></summary>
-public class PatchApplicationreviewsByApplicationIdApiRequest : IPatchApiRequest<System.Collections.Generic.List<string>>
+public class PatchApplicationreviewsByApplicationIdApiRequest : IPatchApiRequest<PatchApplicationReviewResponse>
 {
     public required System.Guid ApplicationId { get; init; }
     public string PatchUrl => $"api/applicationreviews/{ApplicationId}";
-    public System.Collections.Generic.List<string> Data { get; set; } = default!;
+    public PatchApplicationReviewResponse Data { get; set; } = default!;
 }
 
 /// <summary>GET /api/applicationreviews &#x2192; <see cref="GetApplicationReviewResponse"/></summary>
@@ -147,11 +147,11 @@ public record DeleteEmployerProfilesByAccountLegalEntityIdApiRequest(long Accoun
 }
 
 /// <summary>PATCH /api/employer/profiles/{accountLegalEntityId} &#x2192; <see cref="PatchEmployerProfileResponse"/></summary>
-public class PatchEmployerProfilesByAccountLegalEntityIdApiRequest : IPatchApiRequest<System.Collections.Generic.List<string>>
+public class PatchEmployerProfilesByAccountLegalEntityIdApiRequest : IPatchApiRequest<PatchEmployerProfileResponse>
 {
     public required long AccountLegalEntityId { get; init; }
     public string PatchUrl => $"api/employer/profiles/{AccountLegalEntityId}";
-    public System.Collections.Generic.List<string> Data { get; set; } = default!;
+    public PatchEmployerProfileResponse Data { get; set; } = default!;
 }
 
 /// <summary>GET /api/employer/profiles/{accountLegalEntityId}/addresses &#x2192; List&lt;<see cref="EmployerProfileAddress"/>&gt;</summary>
@@ -181,12 +181,12 @@ public record DeleteEmployerProfilesByAccountLegalEntityIdAddressesByIdApiReques
 }
 
 /// <summary>PATCH /api/employer/profiles/{accountLegalEntityId}/addresses/{id} &#x2192; <see cref="EmployerProfileAddress"/></summary>
-public class PatchEmployerProfilesByAccountLegalEntityIdAddressesByIdApiRequest : IPatchApiRequest<System.Collections.Generic.List<string>>
+public class PatchEmployerProfilesByAccountLegalEntityIdAddressesByIdApiRequest : IPatchApiRequest<EmployerProfileAddress>
 {
     public required long AccountLegalEntityId { get; init; }
     public required int Id { get; init; }
     public string PatchUrl => $"api/employer/profiles/{AccountLegalEntityId}/addresses/{Id}";
-    public System.Collections.Generic.List<string> Data { get; set; } = default!;
+    public EmployerProfileAddress Data { get; set; } = default!;
 }
 
 /// <summary>GET /api/notifications/batch/by/date &#x2192; <see cref="GetBatchByDateResponse"/></summary>
@@ -337,11 +337,11 @@ public class PutUserByIdApiRequest : IPutApiRequest<PutUserRequest>
 }
 
 /// <summary>PATCH /api/user/{id} &#x2192; <see cref="RecruitUser"/></summary>
-public class PatchUserByIdApiRequest : IPatchApiRequest<System.Collections.Generic.List<string>>
+public class PatchUserByIdApiRequest : IPatchApiRequest<RecruitUser>
 {
     public required System.Guid Id { get; init; }
     public string PatchUrl => $"api/user/{Id}";
-    public System.Collections.Generic.List<string> Data { get; set; } = default!;
+    public RecruitUser Data { get; set; } = default!;
 }
 
 /// <summary>POST /api/user/by/email &#x2192; <see cref="RecruitUser"/></summary>
@@ -398,11 +398,11 @@ public record DeleteVacanciesByVacancyIdApiRequest(System.Guid VacancyId) : IDel
 }
 
 /// <summary>PATCH /api/vacancies/{vacancyId} &#x2192; <see cref="Vacancy"/></summary>
-public class PatchVacanciesByVacancyIdApiRequest : IPatchApiRequest<System.Collections.Generic.List<string>>
+public class PatchVacanciesByVacancyIdApiRequest : IPatchApiRequest<Vacancy>
 {
     public required System.Guid VacancyId { get; init; }
     public string PatchUrl => $"api/vacancies/{VacancyId}";
-    public System.Collections.Generic.List<string> Data { get; set; } = default!;
+    public Vacancy Data { get; set; } = default!;
 }
 
 /// <summary>GET /api/vacancies/{vacancyReference}/live &#x2192; <see cref="Vacancy"/></summary>
@@ -526,11 +526,11 @@ public record DeleteVacancyreviewsByIdApiRequest(System.Guid Id) : IDeleteApiReq
 }
 
 /// <summary>PATCH /api/vacancyreviews/{id} &#x2192; <see cref="VacancyReview"/></summary>
-public class PatchVacancyreviewsByIdApiRequest : IPatchApiRequest<System.Collections.Generic.List<string>>
+public class PatchVacancyreviewsByIdApiRequest : IPatchApiRequest<VacancyReview>
 {
     public required System.Guid Id { get; init; }
     public string PatchUrl => $"api/vacancyreviews/{Id}";
-    public System.Collections.Generic.List<string> Data { get; set; } = default!;
+    public VacancyReview Data { get; set; } = default!;
 }
 
 /// <summary>GET /api/vacancies/{vacancyReference}/reviews &#x2192; List&lt;<see cref="VacancyReview"/>&gt;</summary>
